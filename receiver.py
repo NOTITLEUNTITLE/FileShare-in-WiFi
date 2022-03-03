1d1f385f419d76744fdec9284eb668e289a502ff
NOTITLEUNTITLE <inopiction@naver.com>
2022-03-03 10:03:41 +0900

    docs add

---
+++ receiver.py
@@ -0,0 +1,4 @@
+from airshare import receiver
+
+text = receiver.receive(code='notitle')
+print(text)
---
+++ sender.py
@@ -0,0 +1,8 @@
+from airshare import sender
+
+
+# 텍스트 전송
+# sender.send_server(code='notitle', text='hello')
+
+# 파일 전송
+sender.send_server(code='notitle', file="pinkoctopus.PNG")
